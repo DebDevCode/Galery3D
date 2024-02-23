0ff9ba200f59f56f99fe494a5aa255742c255d95
Luca <luca.debionne@yahoo.fr>
2024-02-23 05:18:24 +0100

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ const pexel = (id) =>
   `https://images.pexels.com/photos/${id}/pexels-photo-${id}.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260`;
 const images = [
   // Front
-  // { position: [0, 0, 1.5], rotation: [0, 0, 0], url: "/image200.png" },
+  // { position: [0, 0, 1.5], rotation: [0, 0, 0], url: "/eevy-logo.svg" },
   // Back
   { position: [-0.8, 0, -0.6], rotation: [0, 0, 0], url: pexel(416430) },
   { position: [0.8, 0, -0.6], rotation: [0, 0, 0], url: pexel(310452) },
